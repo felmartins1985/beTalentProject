efda606938cddb8ea47381b3c4a94986663a7fc4
Felipe Costa Martins <felmartins1985@gmail.com>
2024-11-17 16:10:23 -0300

    feat: create routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,2 +1,24 @@
 import router from '@adonisjs/core/services/router'
-import { middleware } from './kernel.js'
+// import { middleware } from './kernel.js'
+const UsersController = () => import('#controllers/users_controller')
+const LoginController = () => import('#controllers/login_controller')
+const ProductsController = () => import('#controllers/products_controller')
+const SalesController = () => import('#controllers/sales_controller')
+const ClientsController = () => import('#controllers/clients_controller')
+router.post('/signup', [UsersController, 'store'])
+router.post('/login', [LoginController, 'login'])
+router.post('/sale', [SalesController, 'store'])
+router.group(() => {
+  router.get('/products', [ProductsController, 'index'])
+  router.get('/products/:id', [ProductsController, 'show'])
+  router.post('/products', [ProductsController, 'store'])
+  router.patch('/products/:id', [ProductsController, 'update'])
+  router.delete('/products/:id', [ProductsController, 'destroy'])
+})
+router.group(() => {
+  router.get('/clients', [ClientsController, 'index'])
+  router.get('/clients/:id', [ClientsController, 'show'])
+  router.post('/clients', [ClientsController, 'store'])
+  router.patch('/clients/:id', [ClientsController, 'update'])
+  router.delete('/clients/:id', [ClientsController, 'destroy'])
+})
