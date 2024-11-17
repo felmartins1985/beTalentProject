3b186d3fdf2a680daa0a22d1abbc3b2dd84cfb1e
Felipe Costa Martins <felmartins1985@gmail.com>
2024-11-17 12:36:17 -0300

    feat: create the hooks on the models

--- app/models/product.ts
+++ app/models/product.ts
@@ -1,7 +1,15 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import {
+  BaseModel,
+  beforeDelete,
+  column,
+  hasMany,
+  beforeFind,
+  beforeFetch,
+} from '@adonisjs/lucid/orm'
 import Sale from './sale.js'
 import type { HasMany } from '@adonisjs/lucid/types/relations'
+import type { ModelQueryBuilderContract } from '@adonisjs/lucid/types/model'
 export default class Product extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
@@ -16,10 +24,21 @@ export default class Product extends BaseModel {
   declare price: number
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
-
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
-
+  @column.dateTime({ serializeAs: null })
+  declare deletedAt: DateTime
   @hasMany(() => Sale)
   declare sales: HasMany<typeof Sale>
+
+  @beforeDelete()
+  static async productDeleted(product: Product) {
+    product.deletedAt = DateTime.now()
+    await product.save()
+  }
+  @beforeFind()
+  @beforeFetch()
+  static notShowProductDeleted(search: ModelQueryBuilderContract<typeof Product>) {
+    search.whereNull('deleted_at')
+  }
 }
